9a21e6e290588f5d29d6df6a03e0643a332a0be0
Peter Hoeg <peter@hoeg.com>
2017-12-24 09:03:13 +0800

    wmi: 1.3.14

--- pysamba/talloc.py
+++ pysamba/talloc.py
@@ -1,15 +1,16 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008, Zenoss Inc.
+# Copyright (C) 2008-2010, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
+# under the terms of the GNU General Public License version 2, or (at your
+# option) any later version, as published by the Free Software Foundation.
 #
 # For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
+
 __doc__= "Re-implement the talloc macros in a python-compatible way"
 
 from pysamba.library import library
